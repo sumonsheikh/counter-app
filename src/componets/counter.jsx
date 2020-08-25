6a74fa53fe99aca1fd1f81a063b15e9d1e119a45
sumonsheikh <sm.sumon117@gmail.com>
2020-08-25 19:15:52 +0600

    bind event handler

--- src/componets/counter.jsx
+++ src/componets/counter.jsx
@@ -10,8 +10,9 @@ class Counter extends Component {
         if(this.state.tags.length===0)return <p> "There is no tag"</p>;
         return <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul>;
     };
-    handleIcreament(){
-         console.log("increament clicked");
+    //binding event handler
+    handleIcreament=()=>{
+         console.log("increament clicked",this);
     };
     render() { 
         
