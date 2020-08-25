30ffaad4504255ce61b70eb0802b4d3a448fc472
sumonsheikh <sm.sumon117@gmail.com>
2020-08-25 15:18:18 +0600

    use the attribute

--- src/componets/counter.jsx
+++ src/componets/counter.jsx
@@ -4,14 +4,18 @@ class Counter extends Component {
     //that required of the component
     state = {
         count:0
-      }
+      };
+    styles={
+        fontSize:10,
+        fontWeight:'bold'
+    }
     render() { 
         return(
             //To avoid Extra div use React.Fragment 
             <React.Fragment>
                 {/* add value in this element dynamicly  */}
-                <span>{this.formatCount()}</span>
-                <button>Increament</button>
+                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increament</button>
             </React.Fragment>
         );
     }
