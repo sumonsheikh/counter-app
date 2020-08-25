bcd88868221f324a6d6141e54938671e78923c8c
sumonsheikh <sm.sumon117@gmail.com>
2020-08-25 15:27:18 +0600

    change the attribute dyanamily

--- src/componets/counter.jsx
+++ src/componets/counter.jsx
@@ -3,18 +3,20 @@ class Counter extends Component {
     //state is very special property of react component that contains all data
     //that required of the component
     state = {
-        count:0
+        count:1
       };
     styles={
         fontSize:10,
         fontWeight:'bold'
     }
     render() { 
+        let classes=" badge m-2 badge-";
+        classes += this.state.count===0 ? "warning":"primary";
         return(
             //To avoid Extra div use React.Fragment 
             <React.Fragment>
                 {/* add value in this element dynamicly  */}
-                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span style={this.styles} className={classes}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increament</button>
             </React.Fragment>
         );
