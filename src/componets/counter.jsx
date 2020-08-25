070a9c11a75b99a5b145d5ed10e14dadd70922dd
sumonsheikh <sm.sumon117@gmail.com>
2020-08-25 14:11:11 +0600

    counter component is added

---
+++ src/componets/counter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+    state = {  }
+    render() { 
+        return <h1>Hellow React</h1>;
+    }
+}
+ 
+export default Counter;
