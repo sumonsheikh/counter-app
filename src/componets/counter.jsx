24c96ee1476de51510ff5ad0f221fc79a2ab005b
sumonsheikh <sm.sumon117@gmail.com>
2020-08-25 14:23:57 +0600

    In component use React.Fragment to avoid extra div

--- src/componets/counter.jsx
+++ src/componets/counter.jsx
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {  }
     render() { 
-        return <h1>Hellow React</h1>;
+        return(
+            <React.Fragment>
+                <h1>Hellow React</h1>
+                <button>Increament</button>
+            </React.Fragment>
+        );
     }
 }
  
