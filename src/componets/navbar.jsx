39660393468e62636ad70ba6fe34d2c798800b97
sumonsheikh <sm.sumon117@gmail.com>
2020-08-29 01:22:15 +0600

    Since navbar has no state then we convert to it's stateless component

--- src/componets/navbar.jsx
+++ src/componets/navbar.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 
-
-export default class NavBar extends Component {
-    render() {
-        return (
-          <nav className="navbar navbar-light bg-light">
-              <a href="#" className="navbar-brand">
-        Navbar <span className="badge badge-pill badge-primary">{this.props.totalCounters}</span>
-              </a>
-          </nav>  
-        );
-    }
+const NavBAr = (props) => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <a href="#" className="navbar-brand">
+            Navbar <span className="badge badge-pill badge-primary">
+            {props.totalCounters}</span>
+            </a>
+        </nav>  
+      );
 }
+ 
+export default NavBAr;
+
 
